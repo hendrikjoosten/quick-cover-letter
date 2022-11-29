1a83618edd57cb086d7e51e77de84bdb5f974b91
hendrikjoosten <joostenhendrik@gmail.com>
2022-11-29 23:07:54 +0100

    first tests

--- src/qcl_hj/qcl.py
+++ src/qcl_hj/qcl.py
@@ -1,2 +1,16 @@
-def gen(company):
-    return "i would like to work for " + str(company)
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+import sys
+
+file_name = sys.argv[1]
+
+with open(file_name) as f:
+    lines = f.readlines()
+
+model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-small")
+tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-small")
+
+inputs = tokenizer("summerize" + str(lines), return_tensors="pt")
+
+outputs = model.generate(**inputs)
+
+print(tokenizer.batch_decode(outputs, skip_special_tokens=True))
